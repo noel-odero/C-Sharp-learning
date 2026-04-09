afefec5f402c0e3f9f1b50f8967d7a4ff0d941cf
Noel Odero <anoel.odero@gmail.com>
2026-04-09 08:18:05 +0200

    Add SavingsAccount class with constructor for owner, initial balance, and interest rate

---
+++ BankAccountsApp/SavingsAccount.cs
@@ -0,0 +1,7 @@
+namespace BanksAccountsApp
+{
+    public class SavingsAccount(string owner, decimal initialBalance = 0, decimal interestRate = 0.05m) : BanksAccountsApp(owner, initialBalance)
+    {
+        
+    }
+}
