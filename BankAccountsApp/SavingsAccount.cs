737ceeb6292cca1e5a282016dbeb4bbb8016b1d5
Noel Odero <anoel.odero@gmail.com>
2026-04-09 08:57:42 +0200

    Fix SavingsAccount class constructor and add withdrawal limit properties

--- BankAccountsApp/SavingsAccount.cs
+++ BankAccountsApp/SavingsAccount.cs
@@ -2,6 +2,8 @@ namespace BanksAccountsApp
 {
     public class SavingsAccount(string owner, decimal initialBalance = 0, decimal interestRate = 0.05m) : BanksAccountsApp(owner, initialBalance)
     {
-        
+        public decimal interestRate { get; set;} = interestRate;
+        private int _withdrawalCount = 0; // number of withdrawals
+        private const int MaxWithdrawals = 3; //limit per month
     }
 }
