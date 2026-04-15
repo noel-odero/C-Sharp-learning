1a8d320541c02a3ac6d8f9c5b1e4db631ecdb88c
Noel Odero <anoel.odero@gmail.com>
2026-04-15 15:37:49 +0200

    Implement methods for retrieving and updating TodoItems in TodoManager

--- projects/TodoApp/TodoManager.cs
+++ projects/TodoApp/TodoManager.cs
@@ -25,6 +25,39 @@ namespace TodoListApp
             return true;
         }
 
+        public List<TodoItem> GetAll()
+        {
+            return _items.ToList();
+        }
+
+        public TodoItem? GetById(Guid id)
+        {
+            return _items.FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<TodoItem> GetByStatus(Status status)
+        {
+            return _items.Where(x => x.Status == status).ToList();
+        }
+
+        public List<TodoItem> GetByPriority(Priority priority)
+        {
+            return _items.Where(x => x.Priority == priority ).ToList();
+        }
+
+        public void Update(Guid id, string newTitle, string newDescription, PriorityQueue newPriority)
+        {
+            TodoItem? item = _items.FirstOrDefault(x => x.Id == id);
+            if(item is null)
+            {
+                throw new InvalidOperationException($"Todo with id {id} not found");
+            }
+
+            item.Title = newTitle;
+            item.Description = newDescription;
+            item.Priority = newPriority;
+        }
+
         
     }
 }
