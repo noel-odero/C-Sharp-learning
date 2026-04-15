005f5610971c87f8252b3252d03ff17c359c0fca
Noel Odero <anoel.odero@gmail.com>
2026-04-15 14:28:15 +0200

    add TodoApp project with TodoItem class and enums for task management

---
+++ TodoApp/Todo.cs
@@ -0,0 +1,23 @@
+namespace TodoListApp
+{
+
+    public enum Priority {Low, Medium, High};
+    public enum Status {Pending, InProgress, Done};
+    public class TodoItem(string title, Priority priority,  string description="")
+    {
+        
+        public string Title {get; set;} = title;
+        public string Description{get; set;} = description;
+        public Priority Priority{ get; private set;} = priority;
+        public Status Status{get; private set;} = Status.Pending;
+        public Guid Id{get; private set;} = Guid.NewGuid();
+        public DateTime CreatedAt{get; private set;} = DateTime.Now;
+
+
+        public string GetSummary()
+        {
+            return $"[{Status} {Title} {Priority} - {CreatedAt}]";
+        }
+        
+    }
+}
