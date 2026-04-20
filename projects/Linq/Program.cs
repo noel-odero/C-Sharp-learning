722fa398dd719ad7c3c3be093d667853a7d5f4dd
noel-odero <anoel.odero@gmail.com>
2026-04-20 15:21:14 +0200

    Refactor Program.cs: improve formatting, add missing using directives, and enhance readability

--- projects/Linq/Program.cs
+++ projects/Linq/Program.cs
@@ -1,14 +1,32 @@
-﻿int [] numbers = new[]{2, 3, 4, 5, 3, 6, 7};
-var result = from n in numbers where n < 5 select n;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+int[] numbers = new[] { 2, 3, 4, 5, 3, 6, 7 };
+var result = from n in numbers where n < 5 select n;
 var result2 = numbers.Where(n => n < 5).Select(n => n);
 
-foreach(int i in result2)
+foreach (int i in result2)
 {
     Console.WriteLine(i);
 }
 
-foreach(int i in result)
+foreach (int i in result)
 {
     Console.WriteLine(i);
+}
+
+static class wheres
+{
+    public static IEnumerable<int> Where(this int[] ints, Func<int, bool> gauntlet)
+    {
+        Console.WriteLine("My Where extension");
+        foreach (int i in ints)
+        {
+            if (gauntlet(i))
+            {
+                yield return i;
+            }
+        }
+    }
 }
