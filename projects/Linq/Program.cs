50739e905c814775c00e1e78236760e7c6fb6287
noel-odero <anoel.odero@gmail.com>
2026-04-20 15:01:27 +0200

    Add initial project files for Linq application

---
+++ projects/Linq/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] numbers = new[]{2, 3, 4, 5, 3, 6, 7};
+var result = from n in numbers where n < 5 select n;
+
+var result2 = numbers.Where(n => n < 5).Select(n => n);
+
+foreach(int i in result2)
+{
+    Console.WriteLine(i);
+}
+
+foreach(int i in result)
+{
+    Console.WriteLine(i);
+}
