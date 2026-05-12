e79823faa5e7a271cae869470f9bf1b39da2c39c
noel-odero <anoel.odero@gmail.com>
2026-05-12 06:30:31 +0200

    Add GET endpoint for retrieving todos by ID in AspNetApp

--- projects/AspNetApp/Program.cs
+++ projects/AspNetApp/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -8,6 +10,14 @@ app.MapPost("/todos", (Todo task) =>
 {
     todos.Add(task);
     return TypedResults.Created("/todos/{id}", task);
+});
+
+app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) =>
+{
+    var targetTodo = todos.SingleOrDefault(t => id == t.Id);
+    return targetTodo is null
+    ? TypedResults.NotFound()
+    : TypedResults.Ok(targetTodo);
 });
 
 
