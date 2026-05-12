5375bfebee903100c49db53f9f17c2b52640f749
noel-odero <anoel.odero@gmail.com>
2026-05-12 06:22:14 +0200

    Add HTTP request file and implement todos endpoint in AspNetApp

--- projects/AspNetApp/Program.cs
+++ projects/AspNetApp/Program.cs
@@ -1,8 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var todos = new List<Todo>();
 
+
+app.MapPost("/todos", (Todo task) =>
+{
+    todos.Add(task);
+    return TypedResults.Created("/todos/{id}", task);
+});
 
 
 app.Run();
+
+public record Todo(int Id, string Name, DateTime DueDate, bool IsCompleted);
+
