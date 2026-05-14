76236d26c288731e285240dedd96f444577dc07f
noel-odero <anoel.odero@gmail.com>
2026-05-14 16:01:13 +0200

    feat: Update MailService and MessageService to use VideoEventArgs for video encoding notifications

--- projects/DelegatesAndEvents/MailService.cs
+++ projects/DelegatesAndEvents/MailService.cs
@@ -2,9 +2,9 @@ namespace EnventsAndDelegates
 {
     public class MailService // responsible for sending an email once video is encoded
     {
-        public void OnVideoEncoded(object source, EventArgs e) // handler
+        public void OnVideoEncoded(object source, VideoEventArgs e) // handler
         {
-            Console.WriteLine("MailService: Sending an email...");
+            Console.WriteLine("MailService: Sending an email..." + e.Video.Title);
 
         }
 
--- projects/DelegatesAndEvents/VideoEncoder.cs
+++ projects/DelegatesAndEvents/VideoEncoder.cs
@@ -1,12 +1,16 @@
 namespace EnventsAndDelegates
 {
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }
+    }
     public class VideoEncoder
     {
         // 1- Define a delegate - shape of the method in the subscriber
         // 2 - define an event based on the delegate
         // 3- Raise the event / publish the event - to raise an event, we need a method responsible for it
 
-        public delegate void VideoEncodedHandler(object source, EventArgs args);
+        public delegate void VideoEncodedHandler(object source, VideoEventArgs args);
 
         public event VideoEncodedHandler VideoEncoded;
         public void Encode(Video video )
@@ -14,14 +18,14 @@ namespace EnventsAndDelegates
             Console.WriteLine("Encoding video...");
             Thread.Sleep(3000);
 
-            OnVideoEncoded();
+            OnVideoEncoded(video);
         }
 
-        protected virtual void OnVideoEncoded() // helper, checks if anyone is subscribed then fires the event
+        protected virtual void OnVideoEncoded(Video video) // helper, checks if anyone is subscribed then fires the event
         {
             if(VideoEncoded != null)
             {
-                VideoEncoded(this, EventArgs.Empty);
+                VideoEncoded(this,new VideoEventArgs(){Video = video});
             }
             
         }
