5ba271e153aee610f67a613f65f5d9b52baa8af8
noel-odero <anoel.odero@gmail.com>
2026-05-19 18:37:49 +0200

    feat: Add OrderEventArgs class to handle order event data

---
+++ projects/OrderFlow/Events/OrderEventsArgs.cs
@@ -0,0 +1,20 @@
+namespace OrderFlow.Events;
+
+public class OrderEventArgs : EventArgs // signal that this class carries the event data
+{
+    public Order Order {get; }
+    public string Message {get; }
+    public DateTimeOffset EventTime {get;}
+
+    public OrderEventArgs(Order order, string message)
+    {
+        Order = order;
+        Message = message;
+        EventTime = DateTimeOffset.UtcNow;
+    }
+
+    public override string ToString()
+    {
+        return $"Order placed at: {EventTime}. {Message} -> {Order}";
+    }
+}
