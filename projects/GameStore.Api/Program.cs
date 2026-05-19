65eeb17a813fcf765fedad946b6ab4824e0cf641
noel-odero <anoel.odero@gmail.com>
2026-05-19 15:09:23 +0200

    feat: Update database connection setup in Program.cs for GameStoreContext

--- projects/GameStore.Api/Program.cs
+++ projects/GameStore.Api/Program.cs
@@ -1,11 +1,13 @@
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddValidation();
 
-var connString = "Data Source=GameStore.db";
-builder.Services.AddSqlite<GameStoreContext>(connString);
+var connString = builder.Configuration.GetConnectionString("Default") ?? "Data Source=GameStore.db";
+builder.Services.AddDbContext<GameStoreContext>(options => options.UseSqlite(connString));
+
 var app = builder.Build();
 
 app.MapGamesEndPoint();
