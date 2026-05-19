52d25ce95ffdfea1974e2140dd764132ad8c72ba
noel-odero <anoel.odero@gmail.com>
2026-05-19 19:35:41 +0200

    feat: Implement EmailService for order notifications

---
+++ projects/OrderFlow/Services/EmailService.cs
@@ -0,0 +1,22 @@
+using OrderFlow.Events;
+using OrderFlow.Interfaces;
+
+namespace OrderFlow.Services;
+
+public class EmailService : InotificationService
+{
+    public async Task OnOrderPlacedAsync(object source, OrderEventArgs args)
+    {
+        Console.WriteLine($"Sending confirmation email...");
+        await Task.Delay(1000);
+        Console.WriteLine($"Email sent to {args.Order.Customer.Email}");
+        Console.WriteLine($"Details {args.Order}");
+    }
+     public async Task OnOrderShippedAsync(object source, OrderEventArgs args)
+    {
+        Console.WriteLine($"Sending shippinh notification");
+        await Task.Delay(1000);
+        Console.WriteLine($"Shipping email sent to {args.Order.Customer.Email}");
+        onsole.WriteLine($"Details {args.Order}");
+    }
+}
