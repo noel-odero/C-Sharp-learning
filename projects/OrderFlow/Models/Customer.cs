53eab23eabbe250d9ef76ee687aad7e3588d4c8d
noel-odero <anoel.odero@gmail.com>
2026-05-19 18:04:47 +0200

    feat: Add Customer model to represent customer data structure

---
+++ projects/OrderFlow/Models/Customer.cs
@@ -0,0 +1,20 @@
+namespace OrderFlow.Models;
+
+public class Customer
+{
+    public int Id {get; set;}
+    public string Name {get; set;}
+    public string Email {get; set;}
+
+    public Customer(int id, string name, string email)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+    }
+
+    public override string ToString()
+    {
+        return $"Customer [{Id}] {Name} ({Email})";
+    }
+}
