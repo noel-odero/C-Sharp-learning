bf54789603968e3e4d87166219812e43907a79d1
noel-odero <anoel.odero@gmail.com>
2026-05-20 14:12:16 +0200

    feat: Add AsyncPractice project with tea brewing examples

---
+++ projects/AsyncPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿// Question 1
+async Task BrewTeaAsync()
+{
+    await Task.Delay(3000);
+    Console.WriteLine("Tea is ready");
+
+}
+
+Console.WriteLine("Starting to brew...");
+await BrewTeaAsync();
+Console.WriteLine("Done");
+
+
+
+// Question 2
+async Task<string> BrewTeaAsync1()
+{
+    await Task.Delay(3000);
+    return("Earl grey");
+
+}
+
+string tea = await BrewTeaAsync1();
+Console.WriteLine($"Your tea is {tea}");
